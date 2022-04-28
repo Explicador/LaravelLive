2e9265f0554cc38a549df1688290bcf3c59ffc3e
Jose Seie <joseeugenioseie@hotmail.com>
2022-04-28 14:34:22 +0200

    Initial commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Explicador\E2PaymentsPhpSdk\Mpesa;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function testPackage(Request $request) {
+
+        $mpesa = new Mpesa([
+            'client_secret' => 'vaoB8MLT7PmtpqpqTiOPKHIOJDeqMLRZJPnSSVdU',
+            'client_id'     => '962a48fc-7398-45d6-911d-ee298eb70c49',
+            'wallet_id'     => '943818',
+        ]);
+
+        $responseObject = $mpesa->c2b('846445220', '100', 'TestPagamento');
+
+        dd('Teste de c2b', $responseObject);
+
+    }
+}
